b6bce57fe13cc46428e8802c41f69e77768dd2a0
erwanjb <wawan.jb@gmail.com>
2020-09-30 01:06:52 +0200

    domaine

--- server/config/nodemailer.config.ts
+++ server/config/nodemailer.config.ts
@@ -1,11 +1,14 @@
 import mailer from "nodemailer";
 
-const transport = mailer.createTransport('SMTP', {
+const transport = mailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     service: "Gmail",
     auth: {
         user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD
-    }
+        pass: process.env.MAIL_PASSWORD,
+    },
 });
 
 export default transport;
