1bcf3d68eb5a11bf439c4dc5613006480abcc909
erwanjb <wawan.jb@gmail.com>
2020-09-30 02:07:54 +0200

    googleapis

--- server/config/nodemailer.config.ts
+++ server/config/nodemailer.config.ts
@@ -1,14 +1,32 @@
-import mailer from "nodemailer";
+import { google } from "googleapis";
+import  mailer from "nodemailer";
 
-const transport = mailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    service: "Gmail",
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD,
-    },
+const OAuth2 = google.auth.OAuth2;
+
+const myOAuth2Client = new OAuth2(
+    process.env.CLIENT_ID_GMAIL,
+    process.env.SECRET_GMAIL,
+    "https://developers.google.com/oauthplayground"
+);
+
+myOAuth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN_GMAIL
 });
+
+const transport = async () => {
+    const myAccessToken = await myOAuth2Client.getAccessToken();
+    return mailer.createTransport({
+        service: "GMAIL",
+        auth: {
+            type: "OAuth2",
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD,
+            clientId: process.env.CLIENT_ID_GMAIL,
+            clientSecret: process.env.SECRET_GMAIL,
+            refreshToken: process.env.REFRESH_TOKEN_GMAIL,
+            accessToken: myAccessToken
+        },
+    } as any);
+}
 
 export default transport;
